346c7a518e94d664a7f46b676d87bca010abbe02
Elyavor <55957344+Elyavor@users.noreply.github.com>
2019-12-05 16:12:36 +0300

    Add files via upload

---
+++ Homework05/config.py
@@ -0,0 +1,6 @@
+access_token = '1440b33160742e14418240b5110a86d75905261602ac83e98876d61978dce522d42f924b7c67530dc673f'
+CONNECTED = 200
+domain = "https://api.vk.com/method"
+user_id = 212365683
+fields = 'sex'
+v = '5.103'
